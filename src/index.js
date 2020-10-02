b4e92a8d44876990ea14e92b4a3cb26446744512
Ilya <51325702+treger78@users.noreply.github.com>
2020-10-02 23:32:35 +0300

    feat: make check func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+	let strArr = Array.from(str);
+
+	for (var el = 0; el < strArr.length; el++) {
+		for (var i = 0; i < bracketsConfig.length; i++) {
+			if (strArr[el] === bracketsConfig[i][0] && strArr[el + 1] === bracketsConfig[i][1]) {
+				strArr.splice(el, 2);
+				el -= 2;
+			}
+		}
+	}
+
+	return strArr.length === 0;
+    
 }
